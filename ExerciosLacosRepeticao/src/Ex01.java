f4ac03b8f1a89ab90927e6a1a86c39418bd58236
LUAN FERREIRA BRAZ <luan.braz@alunos.sc.senac.br>
2025-04-11 21:15:21 -0300

    Exercicio 01

---
+++ ExerciosLacosRepeticao/src/Ex01.java
@@ -0,0 +1,13 @@
+package listaexexercicios;
+
+import java.util.Scanner;
+
+public class Ex01 {
+
+    public static void main(String[] args) {
+        
+        for (int i = 1; i <= 10; i++) {
+           System.out.print(i + " "); 
+        }
+    } 
+} 
